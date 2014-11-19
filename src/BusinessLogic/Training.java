75d36e268890138df2f36421916fa90109dee34c
marejada <tormazoff@gmail.com>
2014-11-19 13:48:31 +0300

    comit training

--- src/BusinessLogic/Training.java
+++ src/BusinessLogic/Training.java
@@ -4,10 +4,14 @@ import DataAccess.DBProcessor;
 
 public class Training extends Event {
     private String trainingType;
-    Training (int trainingId) {
+    public Training(int trainingId) {
         isTraining = true;
         if (trainingId != 0) { //уже есть такая
             eventId = trainingId;
+            Training training = DBProcessor.getTraining(this);
+            eventName = training.getEventName();
+            eventDate = training.getEventDate();
+            trainingType = training.getTrainingType();
         }
     }
 
@@ -19,12 +23,15 @@ public class Training extends Event {
         this.trainingType = trainingType;
     }
 
-    public void save () {
-        int newEventId = DBProcessor.newTraining(this);
-        this.eventId = newEventId;
+    public void save (int type) {
+        if (eventId == 0) {
+            int newEventId = DBProcessor.newTraining(this, type);
+            this.eventId = newEventId;
+        }
     }
 
     public void  deleteTraining () {
         DBProcessor.deleteEvent(this);
+
     }
 }
